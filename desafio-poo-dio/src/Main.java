9e2fd8a6baafa89c84db086518ff38b2468275cd
marcospmelo <marcopmelo@gmail.com>
2023-01-27 11:48:22 -0400

    Inclusão Abstraindo um Bootcamp Usando Orientação a Objetos em Java

---
+++ desafio-poo-dio/src/Main.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2= new Curso();
+		curso2.setTitulo("curso javascript");
+		curso2.setDescricao("descrição curso javascript");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria de java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+		
+		/*System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);*/
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devMarcos = new Dev();
+		devMarcos.setNome("Marcos");
+		devMarcos.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Marcos: " + devMarcos.getConteudosInscritos());
+		devMarcos.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Marcos: " + devMarcos.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Marcos: " + devMarcos.getConteudosConcluidos());
+		System.out.println("XP: " + devMarcos.calcularTotalXp());
+		
+		System.out.println("------------------------------------");
+		
+		Dev devPaulo = new Dev();
+		devPaulo.setNome("Paulo");
+		devPaulo.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Paulo: " + devPaulo.getConteudosInscritos());
+		devPaulo.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Paulo: " + devPaulo.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Paulo: " + devPaulo.getConteudosConcluidos());
+		System.out.println("XP: " + devPaulo.calcularTotalXp());
+	}
+}
